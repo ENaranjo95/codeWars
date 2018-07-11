6f7197c29139acca5b86b30b989a5fee21f5f5e7
enaranjo95 <ednara1279@gmail.com>
2018-07-11 16:39:50 -0400

    13 codewar

---
+++ replaceVowels.js
@@ -0,0 +1,15 @@
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+describe("Basic Tests", function(){
+  it("It should works for basic tests", function(){
+
+Test.assertSimilar(replace("Hi!") , "H!!")
+Test.assertSimilar(replace("!Hi! Hi!") , "!H!! H!!")
+Test.assertSimilar(replace("aeiou") , "!!!!!")
+Test.assertSimilar(replace("ABCDE") , "!BCD!")
+
+})})
+
+function replace(s){
+  return s.replace(/[aeiouAEIOU]/gi, "!")
+}
