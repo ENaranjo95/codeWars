bd599faffb6ceca2b8df2a7da244414587717e38
enaranjo95 <ednara1279@gmail.com>
2018-08-02 23:25:43 -0400

    29 codeWar

---
+++ booleanString.js
@@ -0,0 +1,11 @@
+// A function is a block of code that takes an input and produces an output.
+// In this example, boolean_to_string is a function whose input is either true or false,
+// and whose output is the string representation of the input, either "true" or "false".
+
+function booleanToString(b){
+  if(b === true){
+  return 'true'
+  }else{
+  return 'false'
+  }
+}
